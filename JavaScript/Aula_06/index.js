376ae61178dcadf8b7d35ea2f6988023cba89bc0
GuilhermeGomesDosSantos <Gomes3654gui@gmail.com>
2023-07-16 07:47:47 -0300

    Aula 06

---
+++ JavaScript/Aula_06/index.js
@@ -0,0 +1,35 @@
+// Não podemos criar variáveis com palavras reservadas, string, float, number, etc...
+// Variáveis precisam ter nomes significativos
+// Não pode começar o nome de uma variável com um número
+// Não podem conter espaços ou traços
+// Utilizamos camelCase
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
+// NÃO UTILIZE VAR, UTILIZE LET.
+/*let nomeCliente = 'Guilherme';
+nomeCliente = 'Gomes';
+
+
+console.log(nomeCliente);
+
+*/
+
+/*
+let nome = 'Guilherme';
+
+console.log(nome, 'Nasceu em 1984');
+console.log('Em 2000', nome, 'conheceu Maria');
+console.log(nome, 'casou-se com, Maria em 2012');
+console.log('Maria teve 1 filho com', nome, 'em 2015');
+console.log('O filho de', nome, 'se chama Eduardo');
+*/
+/*
+let nome; // Declarou a variável
+nome = 'Gui'// inicializando a variável
+console.log(nome);
+nome = 'Gomes';
+console.log(nome);
+*/
+
+let nomeCompletoDoCliente = 'Guilherme Gomes dos Santos';
+console.log(nomeCompletoDoCliente);
