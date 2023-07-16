44913d21f1b1827f7e213565474d75a21470d839
GuilhermeGomesDosSantos <Gomes3654gui@gmail.com>
2023-07-16 07:39:36 -0300

    Aula 04

---
+++ JavaScript/Aula_04/index.js
@@ -0,0 +1,14 @@
+// E escrever u comentário longo
+
+/*
+Comentario Longo
+Linah de comentário
+*/
+console.log('Olá mundo!'); // aqui tem outro comentário
+console.log('Linha 1'); // aqui tem outro comentário
+// console.log('Linha 2');
+/*
+console.log('Linha 3');
+console.log('Linha 4');
+console.log('Linha 5'); 
+*/
