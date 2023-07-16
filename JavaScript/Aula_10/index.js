429cfd0614830278f13181683f57283555dc6e73
GuilhermeGomesDosSantos <Gomes3654gui@gmail.com>
2023-07-16 07:58:59 -0300

    Aula 10

---
+++ JavaScript/Aula_10/index.js
@@ -0,0 +1,122 @@
+/*Aritméticos
++ Adição
+- subtração
+/ Divisão
+* Multiplicação
+% resto
+*/
+// const num1 = 5;
+// const num2 = 10;
+// console.log(num1 - num2);
+
+// const num1 = 2;
+// const num2 = 10;
+// console.log(num1 ** num2);
+
+// const num1 = 10;
+// const num2 = 5;
+// console.log(num1 % num2);
+
+/*
+()
+**
+* / %
++ -
+*/
+
+// const num1 = 10;
+// const num2 = 5;
+// const num3 = 15;
+// console.log(num1 + num2 * num3);
+// console.log((num1 + num2) * num3);
+// console.log(num1 * num2 / num3);
+
+// let contador = 1;
+// // let contador = 1;// Constante irá dar erro
+// contador++;
+// contador++;
+// contador++;
+// contador++;
+// console.log(contador)
+
+
+// let contador = 1;
+// // let contador = 1;// Constante irá dar erro
+// console.log(contador++);
+// console.log(contador);
+
+// let contador = 1;
+// // let contador = 1;// Constante irá dar erro
+// console.log(++contador);
+
+
+// let contador = 1;
+// // let contador = 1;// Constante irá dar erro
+// console.log(--contador);
+
+const passo = 50;
+let contador = 2;
+
+// contador = contador + passo;
+// console.log(contador);
+
+// contador = contador + passo;
+// console.log(contador);
+
+// contador = contador + passo;
+// console.log(contador);
+
+// contador += 2;
+// contador += 2;
+// contador += 2;
+// console.log(contador);
+
+// contador *= 2;
+// contador *= 2;
+// contador *= 2; 
+// console.log(contador);
+
+
+// contador **= 10;
+// console.log(contador);
+
+// const num1 = 10;
+// const num2 = 'Gui';
+// const num2 = '5';
+// console.log(num1 * num2);
+// console.log(num1 * num2);
+
+// const num1 = 10;
+// const num2 = '5';
+// console.log(num1 + num2);
+// console.log(typeof num2);
+
+// const num1 = 10;
+// const num2 =  parseInt ('5.2'); /*
+// The parseInt() function parses a string argument and returns an integer of the specified
+// radix (the base in mathematical numeral systems).
+// */
+// console.log(num1 + num2);
+// console.log(typeof num2);
+
+// const num1 = 10;
+// const num2 =  parseFloat ('5.2'); /*
+// The parseFloat() function parses a string argument and returns a floating point number.
+// */
+// console.log(num1 + num2);
+// console.log(typeof num2);
+
+// const num1 = 10;
+// const num2 =  Number ('5.2'); /*
+// The parseFloat() function parses a string argument and returns a floating point number.
+// */
+// console.log(num1 + num2);
+// console.log(typeof num2);
+
+
+const num1 = 10;
+const num2 =  Number ('Guilherme'); /*
+The parseFloat() function parses a string argument and returns a floating point number.
+*/
+console.log(num1 + num2);
+console.log(typeof num2);
